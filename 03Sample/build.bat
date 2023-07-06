96cc6d641550400ce076d1d5eb9e4ea5b789fbb0
jiaoweipeng <weipeng.x.jiao@gmail.com>
2023-07-06 23:58:29 +0800

    add DeviceInfo

--- 03Sample/build.bat
+++ 03Sample/build.bat
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019"  -DOpenCV_DIR="D:\tools\OpenCV\opencv\build" ..
+cmake -G "Visual Studio 16 2019" ..
 cmake --build . --config Release -j8
 pause
